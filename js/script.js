22522c73ee5c03592262694161287e8ec884d6ef
cwalseth <walsethcallie@gmail.com>
2023-05-03 19:55:18 -0500

    started animation for home page

--- js/script.js
+++ js/script.js
@@ -11,48 +11,60 @@ $(document).ready(function () {
     const nav = $('.container1 > nav');
     const navLinks = $('.container1 > nav > ul > li');
     const redBox = $('#one');
-    
-    const timeOut1 = setTimeout(preloader, 100);
-    const timeOut2 = setTimeout(postloader, 2200); 
-
-    body.on("load", function () {
+    const main = $("main#main");
+    const container = $("#container");
+    const timeOut1 = setTimeout(preloader, 1000);
+    // const timeOut2 = setTimeout(postloader, 2200); 
+    // main.hide();
+    // container.on("load", function () {
+    //     preloader();
+    // })
+    window.addEventListener("load", function(e) {
         preloader();
     })
-
     function preloader() {
-
-        loading.addClass('displayed');
-        loading.removeClass('nodisplay');
+        // loading.fadeOut(2000);
         let tl = gsap.timeline();
+        tl.fromTo(container,{autoAlpha: 0}, {autoAlpha: 1, duration: 2})
+        tl.to(loading, {autoAlpha: 0, duration: 3}, "+=2")
+        // main.show().fadeIn(2000);
+        tl.fromTo(main, {autoAlpha: 0}, {autoAlpha: 1, duration: 2})
+    }
+
+    // function preloader() {
+
+    //     loading.addClass('displayed');
+    //     loading.removeClass('nodisplay');
+    //     let tl = gsap.timeline();
 
         // gsap.from(
-        tl.from(
-            '.container1', {
-            'backgroundColor': '#000',
-            autoAlpha: 0,
-            y: -200,
-            ease: "bounce.out", 
-            duration: 0.5
-        });
-    };
-    function postloader() {
-        let tl = gsap.timeline({});
-        tl.to(loadingH1, {x:"-200%", duration:1, autoAlpha:0});
-        tl.to(loadingSVG, {y:"-200%", duration:1, autoAlpha:0});
+        // tl.from(
+        //     '.container1', {
+        //     'backgroundColor': '#000',
+        //     autoAlpha: 0,
+        //     y: -200,
+        //     ease: "bounce.out", 
+        //     duration: 0.5
+        // });
+    // };
+    // function postloader() {
+    //     let tl = gsap.timeline({});
+    //     tl.to(loadingH1, {x:"-200%", duration:1, autoAlpha:0});
+    //     tl.to(loadingSVG, {y:"-200%", duration:1, autoAlpha:0});
         
-        tl.to(nav, {autoAlpha:0.75, duration:0.5})
-        tl.from(navLinks, {opacity:0, stagger:0.2})
-        tl.to(loading, {"display":"none", autoAlpha:0, duration:0.2, });
-        loading.remove()
-    }
+    //     tl.to(nav, {autoAlpha:0.75, duration:0.5})
+    //     tl.from(navLinks, {opacity:0, stagger:0.2})
+    //     tl.to(loading, {"display":"none", autoAlpha:0, duration:0.2, });
+    //     loading.remove()
+    // }
     
-tl3.fromTo(redBox, {
-    x:"-100%",
-    backgoundColor: "red",
-    duration: 2
-},{
-    x: "0%",
-    backgoundColor: "blue"
-})
+// tl3.fromTo(redBox, {
+//     x:"-100%",
+//     backgoundColor: "red",
+//     duration: 2
+// },{
+//     x: "0%",
+//     backgoundColor: "blue"
+// })
 
-});
+ });
